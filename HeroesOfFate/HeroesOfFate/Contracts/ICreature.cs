98ed8a3afe095e8c602f980f8297486330696b4c
Ivanov-Stiliqn <stilianivanovivanov@gmail.com>
2015-12-09 20:25:36 +0200

    CREATURE RULZZZZZZZZZZ :D

---
+++ HeroesOfFate/HeroesOfFate/Contracts/ICreature.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace HeroesOfFate.Contracts
+{
+    public interface ICreature
+    {
+        double Damage { get; set; }
+        
+        double Health { get; set; }
+        
+        double Armor { get; set; }
+
+        short Level { get; set; }
+
+        short Exp { get; set; }
+
+        bool IsDead { get; }
+    }
+}
