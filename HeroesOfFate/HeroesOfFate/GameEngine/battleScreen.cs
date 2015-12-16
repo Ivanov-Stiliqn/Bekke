693609fa93a442a41b22fb82e251c31b87c186e1
ygermanov <j_germanov@yahoo.com>
2015-12-16 07:34:06 +0200

    adding battlescreen class for when engaged in battle (actual code is coming soon)

---
+++ HeroesOfFate/HeroesOfFate/GameEngine/battleScreen.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace HeroesOfFate.GameEngine
+{
+    static class battleScreen
+    {
+        public static void drawBattleScreen()
+        {
+            System.Console.Clear();
+            System.Console.WriteLine("you are engad in battle");
+        }
+    }
+}
